14c8eba51b7831d4369f614cc5c70664928012cf
aes258 <abby.spears@yale.edu>
2023-09-06 15:22:40 -0400

    Adding documentation

--- get.data.r
+++ get.data.r
@@ -1,7 +1,9 @@
+## get.data.r
+## Read in cleandata from gym2024data repo with github links 
 
-
-# read in data_2017_2021.csv
+# read in data_2017_2021.csv 
 data_1721 <- read.csv('https://raw.githubusercontent.com/ucsas/gym2024data/main/cleandata/data_2017_2021.csv')
 
 # read in data_2022_2023.csv
 data_2223 <- read.csv('https://raw.githubusercontent.com/ucsas/gym2024data/main/cleandata/data_2022_2023.csv')
+
