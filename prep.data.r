43dabb89c64b7e2975161db1072e4120d5a9d36c
davidmetrick <120599946+davidmetrick@users.noreply.github.com>
2023-09-06 15:02:21 -0400

    Added text from assignment

--- prep.data.r
+++ prep.data.r
@@ -1,1 +1,9 @@
+## prep.data.r
 
+## This script prepares the data from the csv files for modeling
+
+## First, deduplicate names, clean dates, etc
+
+## Then, create some predictors
+
+## etc (add more subheadings for other main tasks)
