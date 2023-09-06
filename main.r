b126a8010e91af1d673d08f868cbe262510f909e
davidmetrick <120599946+davidmetrick@users.noreply.github.com>
2023-09-06 15:00:59 -0400

    Update main.r

--- main.r
+++ main.r
@@ -1,1 +1,13 @@
+## main.r
 
+## This script runs all code related to gymnastics case study.
+
+## It reads data and generates all outputs.
+
+source(’R/get.data.r’)
+
+source(’R/prep.data.r’)
+
+source(’R/fit.model.r’)
+
+## etc
