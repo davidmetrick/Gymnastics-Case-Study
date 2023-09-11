c994330ee9a65a51b222a1dd2d136404ebd2bbe1
davidmetrick <120599946+davidmetrick@users.noreply.github.com>
2023-09-11 15:27:54 -0400

    created gender and apparatus-specific data frames

--- fit.model.r
+++ fit.model.r
@@ -1,1 +1,2 @@
-
+## fit.model.r
+## use cleaned dataset to create scree plot, tier lists, interactive team choices, etc.
--- prep.data.r
+++ prep.data.r
@@ -15,3 +15,28 @@
 # Clean up apparatus names (VT1 VT_1)
 # Separate qualifying & final (or superscore, etc)
 # Do basic analysis based on the team
+
+data_2223
+data_2223$Apparatus <- gsub("hb", "HB", data_2223$Apparatus)
+data_2223$Apparatus <- gsub("VT_1", "VT1", data_2223$Apparatus)
+data_2223$Apparatus <- gsub("VT_2", "VT2", data_2223$Apparatus)
+  
+men <- data_2223[data_2223$Gender == "m",]
+women <- data_2223[data_2223$Gender == "w",]
+
+hb <- men[men$Apparatus == "HB",]
+ph <- men[men$Apparatus == "PH",]
+fx_m <- men[men$Apparatus == "FX",]
+pb <- men[men$Apparatus == "PB",]
+sr <- men[men$Apparatus == "SR",]
+vt1_m <- men[men$Apparatus == "VT1",]
+vt2_m <- men[men$Apparatus == "VT2",]
+vt_m <- men[men$Apparatus == "VT",]
+
+bb <- women[women$Apparatus == "BB",]
+fx_w <- women[women$Apparatus == "FX",]
+ub <- women[women$Apparatus == "UB",]
+vt1_w <- women[women$Apparatus == "VT1",]
+vt2_w <- women[women$Apparatus == "VT2",]
+vt_w <- women[women$Apparatus == "VT",]
+
