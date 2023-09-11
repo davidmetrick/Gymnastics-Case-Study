9c91f971913c396c856296d8df866805f9ab16bb
davidmetrick <120599946+davidmetrick@users.noreply.github.com>
2023-09-11 15:44:01 -0400

    Update fit.model.r

--- fit.model.r
+++ fit.model.r
@@ -1,2 +1,4 @@
 ## fit.model.r
 ## use cleaned dataset to create scree plot, tier lists, interactive team choices, etc.
+
+## first step: make model that finds the average score for each person on each individual apparatus.
