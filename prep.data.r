149ab056e5fae798510c8927f394cb8c859013f9
davidmetrick <120599946+davidmetrick@users.noreply.github.com>
2023-10-16 14:40:33 -0400

    grace mccallum

--- prep.data.r
+++ prep.data.r
@@ -91,6 +91,9 @@ data_2223[data_2223$outlier == 2,]$Country <- "BRA"
 data_2223$outlier <- (data_2223$LastName=="CALLUM") + (data_2223$FirstName=="MC")
 data_2223[data_2223$outlier == 1,]$FirstName <- "GRACE"
 data_2223[data_2223$outlier == 1,]$LastName <- "MCCALLUM"
+data_2223$outlier <- (data_2223$FirstName == "CALLUM") + (data_2223$LastName == "MC")
+data_2223[data_2223$outlier == 2,]$FirstName <- "GRACE"
+data_2223[data_2223$outlier == 2,]$LastName <- "MCCALLUM"
 data_2223 <- subset(data_2223, select = -outlier)
 
 #Remove all duplicated rows
